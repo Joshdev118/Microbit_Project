bb69db51204e3aeffccb379e7958a0bfffa1abf7
Joshdev118 <151276832+Joshdev118@users.noreply.github.com>
2023-12-30 16:06:11 +0800

    last stretch

--- custom.ts
+++ custom.ts
@@ -1,18 +1,33 @@
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon=""
-namespace cool_block{
-    /**
-     * blink diffrent patterns of lights
-     * @param wefwrg
-     * @param ewfwefwe
-     * @param fwfwfwef
-     */
-    //% block="blink x $x y $y every $interval ms"
-    export function blink(x: number, y: number, interval: number): void {
-        let sprite = game.createSprite(4, 4)
-        sprite.setBlink(interval)
-        sprite.blink()
+//% weight=100 color=#3dd4d4 icon=""
+namespace myBlocks{
+
+    //% block="gameOver score: $score"
+    export function gameOver(score: number): void {
+            led.stopAnimation();
+            led.setBrightness(255);
+            while (true) {
+                for (let i = 0; i < 8; i++) {
+                    basic.clearScreen();
+                    basic.pause(100);
+                    basic.showLeds(`1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1`, 300);
+                }
+                basic.showAnimation(`1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 1 1 1 0 0 1 1 0 0 0 1 0 0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 0 1 1 1 0 0 1 1 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+1 1 0 1 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 0 1 1 1 1 0 0 1 1 1 0 0 0 1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 1 1 1 1 0 0 1 1 1 0 0 0 1 1 0 0 0 0 1 0 0 0 0 0`, 100);
+                for (let j = 0; j < 3; j++) {
+                    basic.showString(" GAMEOVER ", 100);
+                    basic.showString("SCORE");
+                    basic.showNumber(score);
+                }
+            }
+        }
     }
-}
--- main.ts
+++ main.ts
@@ -1,16 +1,3 @@
-let playerAlive = 0
-let menu = 0
-let enemy_2: game.LedSprite = null
-let enemy: game.LedSprite = null
-let bullet: game.LedSprite = null
-let bullet_tone = 0
-let wave = 0
-let player: game.LedSprite = null
-let startGame = 0
-let x1
-let x2
-
-
 // player movement A
 input.onButtonPressed(Button.A, function () {
     if (startGame == 1) {
@@ -19,30 +6,26 @@ input.onButtonPressed(Button.A, function () {
         } else {
             player.change(LedSpriteProperty.X, -1)
         }
-        music.play(music.tonePlayable(196, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.InBackground)
+        music.play(music.tonePlayable(156, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+    }else {
+
     }
 })
-// player movement B
-input.onButtonPressed(Button.B, function () {
-    if (startGame == 1) {
-        if (player.get(LedSpriteProperty.X) == 4) {
-            player.set(LedSpriteProperty.X, 0)
-        } else {
-            player.change(LedSpriteProperty.X, 1)
-        }
-        music.play(music.tonePlayable(196, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.InBackground)
-    }
-})
-
-
+function lose () {
+    enemy.delete()
+    enemy_2.delete()
+    player.delete()
+    startGame = 0
+    playerAlive = 0
+    gameOver = 1
+    myBlocks.gameOver(game.score())
+}
 // shooting bullet
 input.onButtonPressed(Button.AB, function () {
     if (playerAlive == 1) {
-        bullet_tone = 988
+        music.play(music.createSoundExpression(WaveShape.Triangle, 2313, 1, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
         bullet = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
         for (let index = 0; index < 5; index++) {
-            music.play(music.tonePlayable(bullet_tone, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
-            bullet_tone += -50
             bullet.change(LedSpriteProperty.Y, -1)
             if (bullet.isTouching(enemy)) {
                 bullet.delete()
@@ -53,13 +36,26 @@ input.onButtonPressed(Button.AB, function () {
                 enemy_2.delete()
                 game.addScore(1)
             }
+            pause(20)
         }
         bullet.delete()
     } else {
     	
     }
 })
+// player movement B
+input.onButtonPressed(Button.B, function () {
+    if (startGame == 1) {
+        if (player.get(LedSpriteProperty.X) == 4) {
+            player.set(LedSpriteProperty.X, 0)
+        } else {
+            player.change(LedSpriteProperty.X, 1)
+        }
+        music.play(music.tonePlayable(156, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+    }else{
 
+    }
+})
 // on_off settings
 input.onGesture(Gesture.Shake, function () {
     // startGame
@@ -69,35 +65,24 @@ input.onGesture(Gesture.Shake, function () {
         startGame = 1
         basic.clearScreen()
         led.stopAnimation()
-        wave_1()
-    } else if (startGame != 0) {
-        menu = 1
-        startGame = 0
-        playerAlive = 0
-        player.delete()
-        wave = 0
+        enemyWave()
+    } else if (startGame == 0 && gameOver == 1) {
+        menu = 0
+        music.ringTone(Note.C)
+        startGame = 1
+        basic.clearScreen()
+        led.stopAnimation()
+        enemyWave()
     } else if (startGame == 0 && menu == 0) {
         menu = 1
     }
 })
-
-
-basic.forever(function () {
-    if (startGame == 0 && menu == 1) {
-        basic.showString("DEFENDER")
-        basic.pause(200)
-    }
-})
-
-
-function wave_1() {
+function enemyWave() {
     basic.pause(100)
-    basic.showString("Wave 1")
     playerAlive = 1
     player = game.createSprite(randint(0, 4), 4)
     game.setScore(0)
-    wave = 1
-    while (wave == 1) {
+    while (game.score() < 20) {
         enemy = game.createSprite(x1 = randint(1, 4), 0)
         enemy_2 = game.createSprite(x2 = randint(1, 4), 0)
         if (x1 == x2) {
@@ -108,6 +93,13 @@ function wave_1() {
         for (let index = 0; index < 4; index++) {
             enemy.change(LedSpriteProperty.Y, 1)
             enemy_2.change(LedSpriteProperty.Y, 1)
+            if (enemy.isTouching(player)) {
+                enemy.delete()
+                lose()
+            } else if (enemy_2.isTouching(player)) {
+                enemy_2.delete()
+                lose()
+            }
             music.play(music.tonePlayable(156, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
             basic.pause(500)
             if (startGame != 1) {
@@ -116,9 +108,27 @@ function wave_1() {
                 break;
             } else if (enemy.isDeleted() && enemy_2.isDeleted()) {
                 break;
+            } else if (enemy.get(LedSpriteProperty.Y) == 4 || enemy_2.get(LedSpriteProperty.Y) == 4) {
+                lose()
             }
         }
         enemy.delete()
         enemy_2.delete()
     }
 }
+let menu = 0
+let enemy_2: game.LedSprite = null
+let enemy: game.LedSprite = null
+let bullet: game.LedSprite = null
+let playerAlive = 0
+let player: game.LedSprite = null
+let startGame = 0
+let gameOver = 0
+let x1
+let x2
+basic.forever(function () {
+    if (startGame == 0 && menu == 1) {
+        basic.showString("DEFENDER")
+        basic.pause(200)
+    }
+})
